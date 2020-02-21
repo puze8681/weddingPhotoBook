713c623d7acc02ce6bdd3905f395ae4819bd122d
Hoosasack <rsef0601@naver.com>
2020-02-21 09:53:30 +0900

    setting gridViewAdapter

---
+++ app/src/main/java/kr/puze/weddingphotobook/ThumbnailActivity.kt
@@ -0,0 +1,12 @@
+package kr.puze.weddingphotobook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThumbnailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thumbnail)
+    }
+}
